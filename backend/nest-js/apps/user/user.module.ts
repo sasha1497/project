3c41af1b6f9930fd3843e48b1ca168b700bceb21
sasha1497 <gvsashanth14@gmail.com>
2025-04-26 12:54:17 +0530

    usermodule dep

--- backend/nest-js/apps/user/user.module.ts
+++ backend/nest-js/apps/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-
+import { DbService } from 'libs/services/src/db.service';
+import { McrudService } from 'libs/services/src/mcurd.service';
 @Module({
+  imports: [], // <-- ADD this
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService,DbService, McrudService],
 })
 export class UserModule {}

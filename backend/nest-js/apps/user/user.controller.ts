e9a86f8ff1ec0385b3b8622073d8de6a0608ce48
sasha1497 <gvsashanth14@gmail.com>
2025-04-26 12:18:23 +0530

    user upsert

--- backend/nest-js/apps/user/user.controller.ts
+++ backend/nest-js/apps/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+// user.controller.ts
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('upsert')
+  async upsertUser(@Body() body: any) {
+    const { id, ...data } = body;
+
+    return this.userService.upsertUser(id, data);
+  }
+}
---
+++ backend/nest-js/libs/services/src/mcurd.service.ts
@@ -0,0 +1,65 @@
+import { Inject, Injectable, forwardRef } from '@nestjs/common';
+import { DbService } from './db.service';
+
+@Injectable()
+export class McrudService {
+    constructor(
+        @Inject(forwardRef(() => DbService)) private readonly db: DbService,
+    ) { }
+
+    create(table, datas, primarykey) {
+
+        if (table && datas) {
+            return this.db.get().table(table).insert(datas).returning(primarykey);
+        }
+    }
+
+
+    update(table, datas, condition) {
+
+        if (table && datas) {
+            return this.db.get().table(table).where(condition).update(datas);
+
+
+        }
+    }
+
+
+    read(select: any = "*", table, condition = {}) {
+        return this.db.get().select(select).from(table).where(condition);
+
+    }
+
+
+    async get(select: any = "*", table, condition = null) {
+        const result = await this.db.get().select(this.db.get().raw(select)).from(table).where(condition).first();
+        return result ?? false;
+    }
+
+
+    delete(table, condition) {
+        return this.db.get().from(table).del().where(condition);
+    }
+
+    async createBatch(table, datas, chunkSize = 100, primarykey) {
+        // const chunkSize = 100;
+        return this.db.get().batchInsert(table, datas, chunkSize);
+        // return this.db.get().batchInsert(table, datas, chunkSize).returning(primarykey);
+    }
+
+
+    readIn(select: any = "*", table, value, condition = []) {
+        return this.db.get().select(select).from(table).whereIn(value, condition);
+
+    }
+
+    deleteBatch(table, ids, primarykey) {
+        return this.db.get().from(table).whereIn(primarykey, ids).del();
+    }
+
+    updateBatch(table, ids,datas, primarykey){
+        return this.db.get().from(table).whereIn(primarykey, ids).update(datas);
+    }
+    
+}
+

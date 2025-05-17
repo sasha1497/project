ac78ace4b9baa0be8b391d8847855359b8046fc0
akash <akashlikely348@gmail.com>
2025-05-17 11:36:54 +0530

    new

--- backend/nest-js/apps/user/user.controller.ts
+++ backend/nest-js/apps/user/user.controller.ts
@@ -1,16 +1,27 @@
 // user.controller.ts
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @Post('upsert')
   async upsertUser(@Body() body: any) {
     const { id, ...data } = body;
-console.log("ji");
+    const result = await this.userService.upsertUser(id, data);
 
-    return this.userService.upsertUser(id, data);
+    if (id) {
+      return {
+        message: 'User updated successfully',
+        data: result,
+      };
+    } else {
+      return {
+        message: 'User created successfully',
+        data: result,
+      };
+    }
   }
+
 }
--- backend/nest-js/apps/user/user.service.ts
+++ backend/nest-js/apps/user/user.service.ts
@@ -4,21 +4,17 @@ import { DbService } from 'libs/services/src/db.service';
 import { McrudService } from 'libs/services/src/mcurd.service';
 @Injectable()
 export class UserService {
-  constructor(private db: DbService, private mcurdSerRef: McrudService) {}
+  constructor(private db: DbService, private mcurdSerRef: McrudService) { }
+
 
   async upsertUser(id: number | undefined, data: any) {
     if (id) {
-      // Try updating existing user
-      return ({
-        where: { id },
-        update: data,
-        create: { ...data, id },
-      });
+      return await this.mcurdSerRef.update('users', data, { id });
     } else {
-      // Create new user
-    //   return this.mcurdSerRef.user.create({
-    //     data,
-    //   });
+      const newId = await this.mcurdSerRef.create('users', data, 'id');
+      return { id: newId, ...data };
     }
   }
+
+
 }
